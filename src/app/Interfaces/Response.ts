0a19a53f28ebe744550fd43522c6cc1fddab3335
Elie Soued <eliegebransoued@gmail.com>
2021-09-13 18:16:03 +0200

    Create a Response interface

---
+++ src/app/Interfaces/Response.ts
@@ -0,0 +1,18 @@
+export interface Response {
+  Countries: [
+    {
+      ID: string;
+      Country: string;
+      CountryCode: string;
+      Slug: string;
+      NewConfirmed: number;
+      TotalConfirmed: number;
+      NewDeaths: number;
+      TotalDeaths: number;
+      NewRecovered: number;
+      TotalRecovered: number;
+      Date: Date;
+      Premium: {};
+    }
+  ];
+}
