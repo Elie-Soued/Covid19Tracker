301ce703676ac790cc699bf7b2776c8cc22b21c9
Elie Soued <eliegebransoued@gmail.com>
2021-09-15 13:18:49 +0200

    added the output values of the date picker

--- src/app/Components/date-picker/date-picker.component.ts
+++ src/app/Components/date-picker/date-picker.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormControl, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-date-picker',
@@ -6,7 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./date-picker.component.css'],
 })
 export class DatePickerComponent implements OnInit {
-  constructor() {}
+  @Output() startDate = new EventEmitter<Date>();
+  @Output() endDate = new EventEmitter<Date>();
 
-  ngOnInit(): void {}
+  today: Date = new Date();
+  dateRange = new FormGroup({
+    start: new FormControl(''),
+    end: new FormControl(''),
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    console.log(this.today);
+  }
+
+  sendStartDate(date: Date) {
+    this.startDate.emit(date);
+    console.log(date);
+  }
+
+  sendEndDate(date: Date) {
+    this.endDate.emit(date);
+    console.log(date);
+  }
 }
