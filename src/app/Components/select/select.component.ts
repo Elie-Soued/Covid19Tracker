375e1a02502e935a1c333e62301ee462da21459b
Elie Soued <eliegebransoued@gmail.com>
2021-09-15 13:17:53 +0200

    Create a select component to be used later in the navbar component

---
+++ src/app/Components/select/select.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-select',
+  templateUrl: './select.component.html',
+  styleUrls: ['./select.component.css'],
+})
+export class SelectComponent implements OnInit {
+  @Input() title?: string;
+  @Input() data?: any;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
