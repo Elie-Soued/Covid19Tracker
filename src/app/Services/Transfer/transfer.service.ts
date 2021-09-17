e92844b06950f31f8e8cb2d00691c6f9e5b8a801
Elie Soued <eliegebransoued@gmail.com>
2021-09-17 11:21:37 +0200

    changing the type of the api response to ResultPerCountry

--- src/app/Services/Transfer/transfer.service.ts
+++ src/app/Services/Transfer/transfer.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { Country } from 'src/app/Interfaces/Country';
+import { ResultPerCountry } from 'src/app/Interfaces/ResultPerCountry';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TransferService {
-  private apiResponse = new BehaviorSubject<Country[]>({} as Country[]);
+  private apiResponse = new BehaviorSubject<ResultPerCountry[]>(
+    {} as ResultPerCountry[]
+  );
   constructor() {}
 
-  sendInfo(data: Country[]) {
+  sendInfo(data: ResultPerCountry[]) {
     this.apiResponse.next(data);
   }
 
