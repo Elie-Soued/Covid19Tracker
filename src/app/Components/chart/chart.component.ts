d8b1853b9ecb781e55e269d63d34aac9dcbca404
Elie Soued <eliegebransoued@gmail.com>
2021-09-19 17:00:39 +0200

    adding console logs

--- src/app/Components/chart/chart.component.ts
+++ src/app/Components/chart/chart.component.ts
@@ -20,8 +20,10 @@ export class ChartComponent implements OnInit, AfterViewInit {
   constructor(private transferService: TransferService) {}
 
   ngOnInit(): void {
+    console.log('inside ngOnInit');
     Chart.register(...registerables);
     this.getApiResponse();
+    console.log('pilou');
   }
 
   getApiResponse() {
@@ -32,22 +34,25 @@ export class ChartComponent implements OnInit, AfterViewInit {
     for (let i = 0; i < apiResponse.length; i++) {
       this.dates.push(apiResponse[i].Date.slice(0, -10));
       this.cases.push(apiResponse[i].Cases);
+      this.title = apiResponse[0].Status;
     }
-
-    console.log(apiResponse);
+    this.apiResponse = apiResponse;
+    console.log(this.apiResponse);
+    console.log(this.dates);
+    console.log(this.cases);
   }
 
   ngAfterViewInit() {
+    console.log('inside ngAfterViewInit');
     this.canvas = this.canvasChart.nativeElement;
     this.ctx = this.canvas.getContext('2d');
-
     const myChart = new Chart(this.ctx, {
       type: 'line',
       data: {
         labels: ['Noni', 'pilou', 'Karl'],
         datasets: [
           {
-            label: '# of Votes',
+            label: 'Pilou',
             data: [10, 20, 30, 40, 50],
           },
         ],
@@ -61,17 +66,12 @@ export class ChartComponent implements OnInit, AfterViewInit {
         },
       },
     });
-
-    this.getApiResponse();
-
+    console.log('pilou');
     const updateChart = () => {
       myChart.data.datasets[0].data = this.cases;
       myChart.data.labels = this.dates;
       myChart.update();
     };
-
     updateChart();
-
-    // this.apiResponse ? updateChart() : console.log('pilou');
   }
 }
