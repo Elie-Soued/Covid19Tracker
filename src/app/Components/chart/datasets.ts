7e9cd327b202ff31c899d2ecc877ce7fc42a3514
Elie Soued <eliegebransoued@gmail.com>
2021-09-28 15:55:39 +0200

    removing the dataset from the chart component to make the code more readable

---
+++ src/app/Components/chart/datasets.ts
@@ -0,0 +1,35 @@
+const datasets = [
+  {
+    label: 'Confirmed',
+    data: [],
+    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+    borderColor: 'rgba(54, 162, 235, 1)',
+    borderWidth: 2,
+  },
+
+  {
+    label: 'Deaths',
+    data: [1],
+    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+    borderColor: 'rgba(255, 99, 132, 1)',
+    borderWidth: 2,
+  },
+
+  {
+    label: 'Active',
+    data: [6],
+    backgroundColor: 'rgba(255, 206, 86, 0.2)',
+    borderColor: 'rgba(255, 206, 86, 1)',
+    borderWidth: 2,
+  },
+
+  {
+    label: 'Recovered',
+    data: [10],
+    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+    borderColor: 'rgba(75, 192, 192, 1)',
+    borderWidth: 2,
+  },
+];
+
+export default datasets;
